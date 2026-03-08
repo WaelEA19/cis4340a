5288315d804c2fcf65eabcb375fcbdd5a7ffdd56
Wael <34816229+WaelEA19@users.noreply.github.com>
2026-03-08 16:49:26 -0400

    Update R07_ERR00_J.java

--- R07_ERR00_J.java
+++ R07_ERR00_J.java
@@ -1,10 +1,10 @@
 public class R07_ERR00_J {
 
-    // NONCOMPLIANT: empty catch block
     public static void main(String[] args) {
         try {
             int x = 5 / 0;
         } catch (Exception e) {
+            System.out.println("Error occurred: " + e.getMessage());
         }
     }
 }
