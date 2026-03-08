e6212c936c4c1df82d23d17ecd3caccf24f41f61
Wael <34816229+WaelEA19@users.noreply.github.com>
2026-03-08 16:38:04 -0400

    Update R02_XP00_J.java

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -2,8 +2,13 @@ import java.io.File;
 
 public class R02_XP00_J {
 
-  public void deleteFile() {
-    File someFile = new File("someFileName.txt");
-    someFile.delete();
-  }
+    public void deleteFile() {
+
+        File someFile = new File("someFileName.txt");
+
+        if (!someFile.delete()) {
+            System.out.println("File deletion failed.");
+        }
+
+    }
 }
