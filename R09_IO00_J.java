cb4c30bb4c33d0915295a4c0a42a8c9b46401aff
Wael <34816229+WaelEA19@users.noreply.github.com>
2026-03-08 16:51:26 -0400

    Update R09_IO00_J.java

--- R09_IO00_J.java
+++ R09_IO00_J.java
@@ -4,7 +4,8 @@ import java.io.IOException;
 public class R09_IO00_J {
 
     public static void main(String[] args) throws IOException {
-        FileInputStream file = new FileInputStream("test.txt");
-        int data = file.read();
+        try (FileInputStream file = new FileInputStream("test.txt")) {
+            int data = file.read();
+        }
     }
 }
