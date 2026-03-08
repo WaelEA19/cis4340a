93c289da50d851d1493206c64618f12865e24fd7
Wael <34816229+WaelEA19@users.noreply.github.com>
2026-03-08 16:42:44 -0400

    Update R00_IDS03_J.java

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -2,13 +2,24 @@ import java.util.logging.Logger;
 import java.util.regex.Pattern;
 
 public class R00_IDS03_J {
-  private static final Logger logger = Logger.getLogger(R00_IDS03_J.class.getName());
 
-  public static void logLogin(boolean loginSuccessful, String username) {
-    if (loginSuccessful) {
-      logger.severe("User login succeeded for: " + username);
-    } else {
-      logger.severe("User login failed for: " + username);
+    private static final Logger logger =
+        Logger.getLogger(R00_IDS03_J.class.getName());
+
+    private static String sanitizeUser(String username) {
+        return Pattern.matches("[A-Za-z0-9_]+", username)
+            ? username
+            : "unauthorized user";
     }
-  }
+
+    public static void logLogin(boolean loginSuccessful, String username) {
+
+        String sanitizedUser = sanitizeUser(username);
+
+        if (loginSuccessful) {
+            logger.severe("User login succeeded for: " + sanitizedUser);
+        } else {
+            logger.severe("User login failed for: " + sanitizedUser);
+        }
+    }
 }
