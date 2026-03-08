648021cb5b9f67eb2c52bd8c86b80960b1ecc2e3
Wael <34816229+WaelEA19@users.noreply.github.com>
2026-03-08 16:40:25 -0400

    Update R06_MET01_J.java

--- R06_MET01_J.java
+++ R06_MET01_J.java
@@ -1,13 +1,18 @@
 public class R06_MET01_J {
 
-  public static int getAbsAdd(int x, int y) {
-    assert x != Integer.MIN_VALUE;
-    assert y != Integer.MIN_VALUE;
+    public static int getAbsAdd(int x, int y) {
 
-    int absX = Math.abs(x);
-    int absY = Math.abs(y);
+        if (x == Integer.MIN_VALUE || y == Integer.MIN_VALUE) {
+            throw new IllegalArgumentException("Invalid input value.");
+        }
 
-    assert (absX <= Integer.MAX_VALUE - absY);
-    return absX + absY;
-  }
+        int absX = Math.abs(x);
+        int absY = Math.abs(y);
+
+        if (absX > Integer.MAX_VALUE - absY) {
+            throw new ArithmeticException("Integer overflow.");
+        }
+
+        return absX + absY;
+    }
 }
