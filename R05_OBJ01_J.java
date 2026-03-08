61f4a1ea146be176fa81aca12c5a6469963dbdce
Wael <34816229+WaelEA19@users.noreply.github.com>
2026-03-08 16:45:51 -0400

    Create R05_OBJ01_J.java

---
+++ R05_OBJ01_J.java
@@ -0,0 +1,11 @@
+public class R05_OBJ01_J {
+
+    public static void main(String[] args) {
+        String a = new String("hello");
+        String b = new String("hello");
+
+        if (a == b) {
+            System.out.println("Equal");
+        }
+    }
+}
