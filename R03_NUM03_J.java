e76dc5523be8ff262ded383d628715e6a93108cf
Wael <34816229+WaelEA19@users.noreply.github.com>
2026-03-08 16:39:07 -0400

    Update R03_NUM03_J.java

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -3,7 +3,9 @@ import java.io.IOException;
 
 public class R03_NUM03_J {
 
-  public static int getInteger(DataInputStream is) throws IOException {
-    return is.readInt();
-  }
+    public static long getInteger(DataInputStream is) throws IOException {
+
+        return is.readInt() & 0xFFFFFFFFL;
+
+    }
 }
